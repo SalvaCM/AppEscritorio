2478352ab0642e91a20c6842ad17a400750b1c64
Salva CM <salvador.cascadoma@elorrieta-errekamari.com>
2019-12-18 12:06:28 +0100

    vista previa mas flujo entre ventanas

--- Alojamientos.vb
+++ Alojamientos.vb
@@ -2,4 +2,9 @@
 	Private Sub Alojamientos_Load(sender As Object, e As EventArgs) Handles MyBase.Load
 
 	End Sub
+
+	Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+		MenuGestion.Show()
+		Me.Hide()
+	End Sub
 End Class
--- Usuarios.vb
+++ Usuarios.vb
@@ -1,3 +1,6 @@
 ﻿Public Class Usuarios
-
+	Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+		MenuGestion.Show()
+		Me.Hide()
+	End Sub
 End Class
