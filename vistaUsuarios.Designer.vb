02b14e5d4506250ac3fd8a77c998bccd6b962f28
AlexLara99 <33617910+AlexLara99@users.noreply.github.com>
2020-01-09 08:11:40 +0100

    cambios de actualizar los datos usuarios

--- vistaUsuarios.Designer.vb
+++ vistaUsuarios.Designer.vb
@@ -41,13 +41,20 @@ Partial Class vistaUsuarios
         Me.btnCancelar = New System.Windows.Forms.Button()
         Me.btnModi = New System.Windows.Forms.Button()
         Me.Label6 = New System.Windows.Forms.Label()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.Label9 = New System.Windows.Forms.Label()
+        Me.ComboBox1 = New System.Windows.Forms.ComboBox()
+        Me.Button1 = New System.Windows.Forms.Button()
         CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
         'Label1
         '
         Me.Label1.AutoSize = True
-        Me.Label1.Location = New System.Drawing.Point(479, 89)
+        Me.Label1.Location = New System.Drawing.Point(550, 139)
         Me.Label1.Name = "Label1"
         Me.Label1.Size = New System.Drawing.Size(60, 13)
         Me.Label1.TabIndex = 1
@@ -56,7 +63,7 @@ Partial Class vistaUsuarios
         'txtNombre
         '
         Me.txtNombre.Enabled = False
-        Me.txtNombre.Location = New System.Drawing.Point(482, 106)
+        Me.txtNombre.Location = New System.Drawing.Point(553, 156)
         Me.txtNombre.Name = "txtNombre"
         Me.txtNombre.Size = New System.Drawing.Size(189, 20)
         Me.txtNombre.TabIndex = 2
@@ -64,7 +71,7 @@ Partial Class vistaUsuarios
         'txtApellidos
         '
         Me.txtApellidos.Enabled = False
-        Me.txtApellidos.Location = New System.Drawing.Point(482, 149)
+        Me.txtApellidos.Location = New System.Drawing.Point(553, 199)
         Me.txtApellidos.Name = "txtApellidos"
         Me.txtApellidos.Size = New System.Drawing.Size(189, 20)
         Me.txtApellidos.TabIndex = 4
@@ -72,7 +79,7 @@ Partial Class vistaUsuarios
         'Label2
         '
         Me.Label2.AutoSize = True
-        Me.Label2.Location = New System.Drawing.Point(479, 132)
+        Me.Label2.Location = New System.Drawing.Point(550, 182)
         Me.Label2.Name = "Label2"
         Me.Label2.Size = New System.Drawing.Size(72, 13)
         Me.Label2.TabIndex = 3
@@ -81,7 +88,7 @@ Partial Class vistaUsuarios
         'txtTelefono
         '
         Me.txtTelefono.Enabled = False
-        Me.txtTelefono.Location = New System.Drawing.Point(482, 192)
+        Me.txtTelefono.Location = New System.Drawing.Point(553, 242)
         Me.txtTelefono.Name = "txtTelefono"
         Me.txtTelefono.Size = New System.Drawing.Size(189, 20)
         Me.txtTelefono.TabIndex = 6
@@ -89,7 +96,7 @@ Partial Class vistaUsuarios
         'Label3
         '
         Me.Label3.AutoSize = True
-        Me.Label3.Location = New System.Drawing.Point(479, 175)
+        Me.Label3.Location = New System.Drawing.Point(550, 225)
         Me.Label3.Name = "Label3"
         Me.Label3.Size = New System.Drawing.Size(70, 13)
         Me.Label3.TabIndex = 5
@@ -98,7 +105,7 @@ Partial Class vistaUsuarios
         'txtDni
         '
         Me.txtDni.Enabled = False
-        Me.txtDni.Location = New System.Drawing.Point(482, 63)
+        Me.txtDni.Location = New System.Drawing.Point(553, 113)
         Me.txtDni.Name = "txtDni"
         Me.txtDni.Size = New System.Drawing.Size(189, 20)
         Me.txtDni.TabIndex = 8
@@ -106,7 +113,7 @@ Partial Class vistaUsuarios
         'Label4
         '
         Me.Label4.AutoSize = True
-        Me.Label4.Location = New System.Drawing.Point(479, 46)
+        Me.Label4.Location = New System.Drawing.Point(550, 96)
         Me.Label4.Name = "Label4"
         Me.Label4.Size = New System.Drawing.Size(32, 13)
         Me.Label4.TabIndex = 7
@@ -115,7 +122,7 @@ Partial Class vistaUsuarios
         'Label5
         '
         Me.Label5.AutoSize = True
-        Me.Label5.Location = New System.Drawing.Point(479, 218)
+        Me.Label5.Location = New System.Drawing.Point(550, 268)
         Me.Label5.Name = "Label5"
         Me.Label5.Size = New System.Drawing.Size(105, 13)
         Me.Label5.TabIndex = 9
@@ -123,7 +130,7 @@ Partial Class vistaUsuarios
         '
         'btnCrear
         '
-        Me.btnCrear.Location = New System.Drawing.Point(697, 59)
+        Me.btnCrear.Location = New System.Drawing.Point(768, 109)
         Me.btnCrear.Name = "btnCrear"
         Me.btnCrear.Size = New System.Drawing.Size(75, 23)
         Me.btnCrear.TabIndex = 11
@@ -132,7 +139,7 @@ Partial Class vistaUsuarios
         '
         'btnModificar
         '
-        Me.btnModificar.Location = New System.Drawing.Point(697, 89)
+        Me.btnModificar.Location = New System.Drawing.Point(768, 139)
         Me.btnModificar.Name = "btnModificar"
         Me.btnModificar.Size = New System.Drawing.Size(75, 23)
         Me.btnModificar.TabIndex = 12
@@ -141,7 +148,7 @@ Partial Class vistaUsuarios
         '
         'btnEliminar
         '
-        Me.btnEliminar.Location = New System.Drawing.Point(697, 119)
+        Me.btnEliminar.Location = New System.Drawing.Point(768, 169)
         Me.btnEliminar.Name = "btnEliminar"
         Me.btnEliminar.Size = New System.Drawing.Size(75, 23)
         Me.btnEliminar.TabIndex = 13
@@ -150,7 +157,7 @@ Partial Class vistaUsuarios
         '
         'Button4
         '
-        Me.Button4.Location = New System.Drawing.Point(697, 526)
+        Me.Button4.Location = New System.Drawing.Point(768, 526)
         Me.Button4.Name = "Button4"
         Me.Button4.Size = New System.Drawing.Size(75, 23)
         Me.Button4.TabIndex = 14
@@ -172,22 +179,28 @@ Partial Class vistaUsuarios
         Me.comboTipoUsuario.Enabled = False
         Me.comboTipoUsuario.FormattingEnabled = True
         Me.comboTipoUsuario.Items.AddRange(New Object() {"Normal", "Administrador"})
-        Me.comboTipoUsuario.Location = New System.Drawing.Point(482, 234)
+        Me.comboTipoUsuario.Location = New System.Drawing.Point(553, 284)
         Me.comboTipoUsuario.Name = "comboTipoUsuario"
         Me.comboTipoUsuario.Size = New System.Drawing.Size(189, 21)
         Me.comboTipoUsuario.TabIndex = 64
         '
         'DataGridView1
         '
+        Me.DataGridView1.BackgroundColor = System.Drawing.SystemColors.ActiveCaption
         Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
-        Me.DataGridView1.Location = New System.Drawing.Point(17, 59)
+        Me.DataGridView1.Location = New System.Drawing.Point(17, 128)
         Me.DataGridView1.Name = "DataGridView1"
-        Me.DataGridView1.Size = New System.Drawing.Size(444, 490)
+        Me.DataGridView1.ReadOnly = True
+        Me.DataGridView1.RowHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.[Single]
+        Me.DataGridView1.RowHeadersVisible = False
+        Me.DataGridView1.ShowCellToolTips = False
+        Me.DataGridView1.ShowEditingIcon = False
+        Me.DataGridView1.Size = New System.Drawing.Size(505, 421)
         Me.DataGridView1.TabIndex = 65
         '
         'btnCancelar
         '
-        Me.btnCancelar.Location = New System.Drawing.Point(482, 271)
+        Me.btnCancelar.Location = New System.Drawing.Point(553, 321)
         Me.btnCancelar.Name = "btnCancelar"
         Me.btnCancelar.Size = New System.Drawing.Size(79, 22)
         Me.btnCancelar.TabIndex = 66
@@ -197,7 +210,7 @@ Partial Class vistaUsuarios
         '
         'btnModi
         '
-        Me.btnModi.Location = New System.Drawing.Point(596, 270)
+        Me.btnModi.Location = New System.Drawing.Point(667, 320)
         Me.btnModi.Name = "btnModi"
         Me.btnModi.Size = New System.Drawing.Size(75, 23)
         Me.btnModi.TabIndex = 67
@@ -208,18 +221,84 @@ Partial Class vistaUsuarios
         'Label6
         '
         Me.Label6.AutoSize = True
-        Me.Label6.Location = New System.Drawing.Point(694, 152)
+        Me.Label6.Location = New System.Drawing.Point(778, 206)
         Me.Label6.Name = "Label6"
         Me.Label6.Size = New System.Drawing.Size(65, 13)
         Me.Label6.TabIndex = 68
         Me.Label6.Text = "modi o crear"
         Me.Label6.Visible = False
         '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Location = New System.Drawing.Point(14, 62)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(60, 13)
+        Me.Label7.TabIndex = 69
+        Me.Label7.Text = "NOMBRE :"
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(137, 59)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(120, 20)
+        Me.TextBox1.TabIndex = 70
+        '
+        'Label8
+        '
+        Me.Label8.AutoSize = True
+        Me.Label8.Location = New System.Drawing.Point(263, 62)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(72, 13)
+        Me.Label8.TabIndex = 71
+        Me.Label8.Text = "APELLIDOS :"
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(341, 59)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(100, 20)
+        Me.TextBox2.TabIndex = 72
+        '
+        'Label9
+        '
+        Me.Label9.AutoSize = True
+        Me.Label9.Location = New System.Drawing.Point(14, 90)
+        Me.Label9.Name = "Label9"
+        Me.Label9.Size = New System.Drawing.Size(105, 13)
+        Me.Label9.TabIndex = 73
+        Me.Label9.Text = "TIPO DE USUARIO:"
+        '
+        'ComboBox1
+        '
+        Me.ComboBox1.FormattingEnabled = True
+        Me.ComboBox1.Items.AddRange(New Object() {"Admin", "Normal"})
+        Me.ComboBox1.Location = New System.Drawing.Point(137, 87)
+        Me.ComboBox1.Name = "ComboBox1"
+        Me.ComboBox1.Size = New System.Drawing.Size(120, 21)
+        Me.ComboBox1.TabIndex = 74
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(366, 87)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 75
+        Me.Button1.Text = "BUSCAR"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
         'vistaUsuarios
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(784, 561)
+        Me.ClientSize = New System.Drawing.Size(880, 561)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.ComboBox1)
+        Me.Controls.Add(Me.Label9)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.Label8)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Label7)
         Me.Controls.Add(Me.Label6)
         Me.Controls.Add(Me.btnModi)
         Me.Controls.Add(Me.btnCancelar)
@@ -239,6 +318,7 @@ Partial Class vistaUsuarios
         Me.Controls.Add(Me.Label2)
         Me.Controls.Add(Me.txtNombre)
         Me.Controls.Add(Me.Label1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
         Me.Name = "vistaUsuarios"
         Me.Text = "Usuarios"
         CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
@@ -265,4 +345,11 @@ Partial Class vistaUsuarios
     Friend WithEvents btnCancelar As Button
     Friend WithEvents btnModi As Button
     Friend WithEvents Label6 As Label
+    Friend WithEvents Label7 As Label
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Label8 As Label
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents Label9 As Label
+    Friend WithEvents ComboBox1 As ComboBox
+    Friend WithEvents Button1 As Button
 End Class
--- vistaUsuarios.vb
+++ vistaUsuarios.vb
@@ -2,7 +2,8 @@
 
 Public Class vistaUsuarios
     Public con As New MySqlConnection("Server=192.168.101.24; Database=alojamientos; Uid=grupoAlojamientos; Pwd=123456")
-    Public adapter As New MySqlDataAdapter("SELECT * FROM TUSUARIOS", con)
+    Public adapter As New MySqlDataAdapter("SELECT cDni'DNI', cApellidos'Apellidos', cNombre'Nombre', cTelefono'Telefono', cTipoUsuario'Tipo' FROM TUSUARIOS", con)
+    'Volver
     Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
         MenuGestion.Show()
         Me.Close()
@@ -14,55 +15,62 @@ Public Class vistaUsuarios
         DataGridView1.DataSource = tabla
         con.Close()
         txtDni.Text = DataGridView1.Rows(0).Cells(0).Value
-        txtNombre.Text = DataGridView1.Rows(0).Cells(3).Value
+        txtNombre.Text = DataGridView1.Rows(0).Cells(2).Value
         txtApellidos.Text = DataGridView1.Rows(0).Cells(1).Value
-        txtTelefono.Text = DataGridView1.Rows(0).Cells(4).Value
-        comboTipoUsuario.Text = DataGridView1.Rows(0).Cells(5).Value
+        txtTelefono.Text = DataGridView1.Rows(0).Cells(3).Value
+        comboTipoUsuario.Text = DataGridView1.Rows(0).Cells(4).Value
 
     End Sub
     'CUANDO HAGAS CLICK EN UNA CELL, QUE SE PONGA EN LOS TEXTBOX LOS DATOS
     Private Sub DataGridView1_CellClick(sender As Object, e As DataGridViewCellEventArgs) Handles DataGridView1.CellClick
         Dim indiceSelect As Integer = DataGridView1.SelectedCells.Item(0).RowIndex
         txtDni.Text = DataGridView1.Rows(indiceSelect).Cells(0).Value
-        txtNombre.Text = DataGridView1.Rows(indiceSelect).Cells(3).Value
+        txtNombre.Text = DataGridView1.Rows(indiceSelect).Cells(2).Value
         txtApellidos.Text = DataGridView1.Rows(indiceSelect).Cells(1).Value
-        txtTelefono.Text = DataGridView1.Rows(indiceSelect).Cells(4).Value
-        comboTipoUsuario.Text = DataGridView1.Rows(indiceSelect).Cells(5).Value
+        txtTelefono.Text = DataGridView1.Rows(indiceSelect).Cells(3).Value
+        comboTipoUsuario.Text = DataGridView1.Rows(indiceSelect).Cells(4).Value
 
     End Sub
 
     'ELIMINAR UN USUARIO
     Private Sub Button3_Click(sender As Object, e As EventArgs) Handles btnEliminar.Click
-        Dim result As DialogResult = MessageBox.Show("Estas seguro que quieres eliminar este usuario?", "Title", MessageBoxButtons.YesNo)
+
         Dim indiceSelect As Integer = DataGridView1.SelectedCells.Item(0).RowIndex
         Dim query As String
         txtDni.Text = DataGridView1.Rows(indiceSelect).Cells(0).Value
-        If (result = DialogResult.Yes) Then
-            Try
-                query = "DELETE FROM tusuarios WHERE cDni = '" & DataGridView1.Rows(indiceSelect).Cells(0).Value & "'"
-                con.Open()
-                Dim mysc As New MySqlCommand(query, con)
-                mysc.ExecuteNonQuery()
-                MessageBox.Show("Usuario eliminado!")
-                con.Close()
-            Catch ex As Exception
-                MessageBox.Show(ex.Message)
-            End Try
-
+        If txtDni.Text = "" Then
+            MsgBox("No tienes seleccionado un usuario")
 
         Else
-            MessageBox.Show("nop")
+            Dim result As DialogResult = MessageBox.Show("Estas seguro que quieres eliminar este usuario?", "Atencion", MessageBoxButtons.YesNo)
+            If (result = DialogResult.Yes) Then
+                Try
+                    query = "DELETE FROM tusuarios WHERE cDni = '" & DataGridView1.Rows(indiceSelect).Cells(0).Value & "'"
+                    con.Open()
+                    Dim mysc As New MySqlCommand(query, con)
+                    mysc.ExecuteNonQuery()
+                    MessageBox.Show("Usuario dado de baja")
+                    con.Close()
+                Catch ex As Exception
+                    MessageBox.Show(ex.Message)
+                End Try
+
+            End If
+            cargaGrid()
         End If
-        cargaGrid()
+
     End Sub
 
     'MODIFICAR UN USUARIO
     Private Sub Button2_Click(sender As Object, e As EventArgs) Handles btnModificar.Click
 
+        If txtDni.Text = "" Then
+            MsgBox("No tienes seleccionado un usuario")
+        Else
+            habilitarTxt()
+            Label6.Text = "Modificar"
+        End If
 
-        habilitarTxt()
-
-        Label6.Text = "Modificar"
 
     End Sub
 
@@ -72,13 +80,9 @@ Public Class vistaUsuarios
 
         habilitarTxt()
         txtDni.Text = Nothing
-
         txtNombre.Text = Nothing
-
         txtApellidos.Text = Nothing
-
         txtTelefono.Text = Nothing
-
         Label6.Text = "Crear"
 
     End Sub
@@ -88,36 +92,40 @@ Public Class vistaUsuarios
         Dim READER As MySqlDataReader
         Dim query As String
         Dim Command As MySqlCommand
-        Try
-            con.Open()
-            If Label6.Text = "Modificar" Then
-                Dim indiceSelect As Integer = DataGridView1.SelectedCells.Item(0).RowIndex
+        Dim result As DialogResult = MessageBox.Show("Estas seguro que quieres guardar los datos este usuario?", "Atencion", MessageBoxButtons.YesNo)
 
-                Dim dni = txtDni.Text
-                Dim nombre = txtNombre.Text
-                Dim apellidos = txtApellidos.Text
-                Dim telefono = txtTelefono.Text
-                Dim tipo = comboTipoUsuario.Text
+        If result = DialogResult.Yes Then
+            Try
+                con.Open()
+                If Label6.Text = "Modificar" Then
+                    Dim indiceSelect As Integer = DataGridView1.SelectedCells.Item(0).RowIndex
 
-                query = "UPDATE tusuarios SET cDni = '" & dni & "', cNombre = '" & nombre & "', cApellidos = '" & apellidos & "', cTelefono = " & telefono & ", cTipoUsuario = '" & tipo & "' "
-                query = query + "WHERE cDni = '" & DataGridView1.Rows(indiceSelect).Cells(0).Value & "'"
-            Else
-                query = "insert into tusuarios values ('" & txtDni.Text & "','" & txtApellidos.Text & "','hola','" & txtNombre.Text & "'," & txtTelefono.Text & ",' " & comboTipoUsuario.Text & "')"
-            End If
-            Command = New MySqlCommand(query, con)
+                    Dim dni = txtDni.Text
+                    Dim nombre = txtNombre.Text
+                    Dim apellidos = txtApellidos.Text
+                    Dim telefono = txtTelefono.Text
+                    Dim tipo = comboTipoUsuario.Text
 
-            READER = Command.ExecuteReader
+                    query = "UPDATE tusuarios SET cDni = '" & dni & "', cNombre = '" & nombre & "', cApellidos = '" & apellidos & "', cTelefono = " & telefono & ", cTipoUsuario = '" & tipo & "' "
+                    query = query + "WHERE cDni = '" & DataGridView1.Rows(indiceSelect).Cells(0).Value & "'"
+                Else
+                    query = "insert into tusuarios values ('" & txtDni.Text & "','" & txtApellidos.Text & "','hola','" & txtNombre.Text & "'," & txtTelefono.Text & ",' " & comboTipoUsuario.Text & "')"
+                End If
+                Command = New MySqlCommand(query, con)
 
-            MessageBox.Show("Usuario guardado con exito!")
+                READER = Command.ExecuteReader
 
+                MessageBox.Show("Usuario guardado con exito!")
+                con.Close()
 
-            con.Close()
+            Catch ex As MySqlException
+                MessageBox.Show(ex.Message)
+            End Try
+            cargaGrid()
+            deshabilitarTxt()
 
-        Catch ex As MySqlException
-            MessageBox.Show(ex.Message)
-        End Try
-        cargaGrid()
-        deshabilitarTxt()
+        End If
+
     End Sub
     'BOTON CANCELAR
     Private Sub BtnCancelar_Click(sender As Object, e As EventArgs) Handles btnCancelar.Click
@@ -155,7 +163,6 @@ Public Class vistaUsuarios
         btnCrear.Enabled = False
         btnModificar.Enabled = False
         btnEliminar.Enabled = False
-
         btnCancelar.Visible = True
         btnModi.Visible = True
         comboTipoUsuario.Enabled = True
