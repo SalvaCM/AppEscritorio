9e5049b10e4c148292501127a663d828b6cbe2b3
AlexLara99 <33617910+AlexLara99@users.noreply.github.com>
2020-01-13 11:55:55 +0100

    informe reservas y cambio de rutas

--- MenuGestion.vb
+++ MenuGestion.vb
@@ -15,8 +15,8 @@ Public Class MenuGestion
 		vistaReservas.Show()
 		Me.Hide()
 	End Sub
-
-	Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click ' Informe alojamientos '
+    ' INFORME ALOJAMIENTOS
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click ' Informe alojamientos '
 		Dim oExcel As Object
 		Dim oBook As Object
 		Dim oSheet As Object
@@ -35,35 +35,23 @@ Public Class MenuGestion
 		oSheet.Columns("J").ColumnWidth = oSheet.Columns("J").ColumnWidth * 2
 		oSheet.Columns("L").ColumnWidth = oSheet.Columns("L").ColumnWidth * 2
 
-		'Ponemos la parte de arriba de la tabla
-		oSheet.Range("A1").Value = "CODIGO"
-		oSheet.Range("A1").Interior.Color = Color.BlanchedAlmond
-		oSheet.Range("B1").Value = "NOMBRE"
-		oSheet.Range("B1").Interior.Color = Color.BlanchedAlmond
-		oSheet.Range("C1").Value = "DESCRIPCION"
-		oSheet.Range("C1").Interior.Color = Color.BlanchedAlmond
-		oSheet.Range("D1").Value = "DIRECCION"
-		oSheet.Range("D1").Interior.Color = Color.BlanchedAlmond
-		oSheet.Range("E1").Value = "EMAIL"
-		oSheet.Range("E1").Interior.Color = Color.BlanchedAlmond
-		oSheet.Range("F1").Value = "LONGITUD"
-		oSheet.Range("F1").Interior.Color = Color.BlanchedAlmond
-		oSheet.Range("G1").Value = "LATITUD"
-		oSheet.Range("G1").Interior.Color = Color.BlanchedAlmond
-		oSheet.Range("H1").Value = "LOCALIDAD"
-		oSheet.Range("H1").Interior.Color = Color.BlanchedAlmond
-		oSheet.Range("I1").Value = "LOCALIZACION"
-		oSheet.Range("I1").Interior.Color = Color.BlanchedAlmond
-		oSheet.Range("J1").Value = "TIPO"
-		oSheet.Range("J1").Interior.Color = Color.BlanchedAlmond
-		oSheet.Range("K1").Value = "TELEFONO"
-		oSheet.Range("K1").Interior.Color = Color.BlanchedAlmond
-		oSheet.Range("L1").Value = "WEB"
-		oSheet.Range("L1").Interior.Color = Color.BlanchedAlmond
-		oSheet.Range("M1").Value = "CAPACIDAD"
-		oSheet.Range("M1").Interior.Color = Color.BlanchedAlmond
+        'Ponemos la parte de arriba de la tabla
+        oSheet.Range("A1").Value = "CODIGO"
+        oSheet.Range("B1").Value = "NOMBRE"
+        oSheet.Range("C1").Value = "DESCRIPCION"
+        oSheet.Range("D1").Value = "DIRECCION"
+        oSheet.Range("E1").Value = "EMAIL"
+        oSheet.Range("F1").Value = "LONGITUD"
+        oSheet.Range("G1").Value = "LATITUD"
+        oSheet.Range("H1").Value = "LOCALIDAD"
+        oSheet.Range("I1").Value = "LOCALIZACION"
+        oSheet.Range("J1").Value = "TIPO"
+        oSheet.Range("K1").Value = "TELEFONO"
+        oSheet.Range("L1").Value = "WEB"
+        oSheet.Range("M1").Value = "CAPACIDAD"
 
-		oSheet.Range("A1:M1").Font.Bold = True
+        oSheet.Range("A1:M1").Interior.Color = Color.BlanchedAlmond
+        oSheet.Range("A1:M1").Font.Bold = True
 
 		Dim con As String = "Server=192.168.101.24; Database=alojamientos; Uid=grupoAlojamientos; Pwd=123456"
 		Dim query As String = "SELECT cCodAlojamiento'CODIGO',cNombre'NOMBRE',cDescripcion'DESCRIPCION',cDireccion'DIRECCION',cEmail'EMAIL',cLongitud'LONGITUD',cLatitud'LATITUD',cLocalidad'LOCALIDAD',cLocalizacion'LOCALIZACION',cTipo'TIPO',cTelefono'TELEFONO',cWeb'WEB',cCapacidad'CAPACIDAD' FROM TALOJAMIENTOS"
@@ -105,14 +93,14 @@ Public Class MenuGestion
 		End Using
 
 
-		'Guardar los elementos en el xlsx
-		oBook.SaveAs("C:\Users\IN1DM3B_09\Desktop\raro\prueba.xlsx")
-		oExcel.Quit
+        'Guardar los elementos en el xlsx
+        oBook.SaveAs("InformeAlojamientos.xlsx")
+        oExcel.Quit
 
-		MsgBox("Informe Generado!")
-	End Sub
-
-	Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        MsgBox("Informe Generado! El archivo se guardara en la carpeta de Documentos")
+    End Sub
+    ' INFORME USUARIOS
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
 		Dim oExcel As Object
 		Dim oBook As Object
 		Dim oSheet As Object
@@ -121,17 +109,26 @@ Public Class MenuGestion
 		oExcel = CreateObject("Excel.Application")
 		oBook = oExcel.Workbooks.Add
 
-		'Add data to cells of the first worksheet in the new workbook    
-		oSheet = oBook.Worksheets(1)
-		'Ponemos la parte de arriba de la tabla
-		oSheet.Range("A1").Value = "DNI"
+        'Add data to cells of the first worksheet in the new workbook    
+        oSheet = oBook.Worksheets(1)
+
+        ' Ampliar el tamaño de las columnas '
+        oSheet.Columns("A").ColumnWidth = oSheet.Columns("B").ColumnWidth * 3
+        oSheet.Columns("B").ColumnWidth = oSheet.Columns("C").ColumnWidth * 3
+        oSheet.Columns("C").ColumnWidth = oSheet.Columns("D").ColumnWidth * 3
+        oSheet.Columns("D").ColumnWidth = oSheet.Columns("H").ColumnWidth * 2
+        oSheet.Columns("E").ColumnWidth = oSheet.Columns("J").ColumnWidth * 2
+
+        'Ponemos la parte de arriba de la tabla
+
+        oSheet.Range("A1").Value = "DNI"
 		oSheet.Range("B1").Value = "Nombre"
 		oSheet.Range("C1").Value = "Apellidos"
 		oSheet.Range("D1").Value = "Telefono"
 		oSheet.Range("E1").Value = "Apellidos"
 		oSheet.Range("A1:E1").Font.Bold = True
-
-		Dim con As String = "Server=192.168.101.24; Database=alojamientos; Uid=grupoAlojamientos; Pwd=123456"
+        oSheet.Range("A1:E1").Interior.Color = Color.BlanchedAlmond
+        Dim con As String = "Server=192.168.101.24; Database=alojamientos; Uid=grupoAlojamientos; Pwd=123456"
 		Dim query As String = "SELECT * FROM tUsuarios"
 		Using sqlConn As New MySqlConnection(con)
 			Using sqlComm As New MySqlCommand()
@@ -161,10 +158,80 @@ Public Class MenuGestion
 		End Using
 
 
-		'Guardar los elementos en el xlsx
-		oBook.SaveAs("C:\Users\IN1DM3B_09\Desktop\prueba.xlsx")
-		oExcel.Quit
+        'Guardar los elementos en el xlsx
+        oBook.SaveAs("InformeUsuarios.xlsx")
+        oExcel.Quit
 
-		MsgBox("Informe Generado!")
-	End Sub
+        MsgBox("Informe Generado! El archivo se guardara en la carpeta de Documentos")
+    End Sub
+    'INFORME RESERVAS
+    Private Sub Button5_Click(sender As Object, e As EventArgs) Handles Button5.Click
+
+        Dim oExcel As Object
+        Dim oBook As Object
+        Dim oSheet As Object
+        Dim contador As Integer = 2
+        'Start a new workbook in Excel    
+        oExcel = CreateObject("Excel.Application")
+        oBook = oExcel.Workbooks.Add
+
+        'Add data to cells of the first worksheet in the new workbook    
+        oSheet = oBook.Worksheets(1)
+
+        ' Ampliar el tamaño de las columnas '
+        oSheet.Columns("A").ColumnWidth = oSheet.Columns("A").ColumnWidth * 3
+        oSheet.Columns("B").ColumnWidth = oSheet.Columns("B").ColumnWidth * 3
+        oSheet.Columns("C").ColumnWidth = oSheet.Columns("C").ColumnWidth * 3
+        oSheet.Columns("D").ColumnWidth = oSheet.Columns("D").ColumnWidth * 2
+        oSheet.Columns("E").ColumnWidth = oSheet.Columns("E").ColumnWidth * 2
+        oSheet.Columns("F").ColumnWidth = oSheet.Columns("F").ColumnWidth * 2
+
+        'Ponemos la parte de arriba de la tabla
+
+        oSheet.Range("A1").Value = "Codigo Reserva"
+        oSheet.Range("B1").Value = "Codigo Alojamiento"
+        oSheet.Range("C1").Value = "DNI"
+        oSheet.Range("D1").Value = "Fecha Entrada"
+        oSheet.Range("E1").Value = "Fecha Realizada"
+        oSheet.Range("F1").Value = "Fecha Salida"
+        oSheet.Range("A1:F1").Font.Bold = True
+        oSheet.Range("A1:F1").Interior.Color = Color.BlanchedAlmond
+        Dim con As String = "Server=192.168.101.24; Database=alojamientos; Uid=grupoAlojamientos; Pwd=123456"
+        Dim query As String = "SELECT * FROM treservas"
+        Using sqlConn As New MySqlConnection(con)
+            Using sqlComm As New MySqlCommand()
+                With sqlComm
+                    .Connection = sqlConn
+                    .CommandText = query
+                    .CommandType = CommandType.Text
+                End With
+                Try
+                    sqlConn.Open()
+                    Dim sqlReader As MySqlDataReader = sqlComm.ExecuteReader()
+                    'vamos leyendo en la base de datos e introduciendo los elementos en el excel
+                    While sqlReader.Read()
+
+                        oSheet.Range("A" & contador).Value = sqlReader("cReserva").ToString()
+                        oSheet.Range("B" & contador).Value = sqlReader("cCodAlojamiento").ToString()
+                        oSheet.Range("C" & contador).Value = sqlReader("cCodUsuario").ToString()
+                        oSheet.Range("D" & contador).Value = sqlReader("cFechaEntrada").ToString()
+                        oSheet.Range("E" & contador).Value = sqlReader("cFechaRealizada").ToString()
+                        oSheet.Range("F" & contador).Value = sqlReader("cFechaSalida").ToString()
+                        contador = contador + 1
+
+                    End While
+                Catch ex As MySqlException
+                    ' add your exception here '
+                End Try
+            End Using
+        End Using
+
+
+        'Guardar los elementos en el xlsx
+        oBook.SaveAs("InformeReservas.xlsx")
+        oExcel.Quit
+
+        MsgBox("Informe Generado! El archivo se guardara en la carpeta de Documentos")
+
+    End Sub
 End Class
